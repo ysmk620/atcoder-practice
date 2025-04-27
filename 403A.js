d29fe0720150b46ee04cc58e162bd0756a7be367
ysmk <0620komako@gmail.com>
2025-04-27 22:14:30 +0900

    clear!

---
+++ 403A.js
@@ -0,0 +1,19 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+var lines = [];
+var reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+reader.on("line", (line) => {
+    lines.push(line);
+});
+reader.on("close", () => {
+    const n = parseInt(lines[0]); 
+    const a = lines[1].split(" ").map(Number);
+    let sum = 0;
+    for (let i = 0; i < n; i += 2) { 
+        sum += a[i];
+    }
+    console.log(sum);
+});
