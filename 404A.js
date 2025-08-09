e5be163da637a6a73558ba4ef5dfa3e72ae7500f
ysmk <0620komako@gmail.com>
2025-08-09 20:09:03 +0900

    clear!

---
+++ 404A.js
@@ -0,0 +1,48 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+var lines = [];
+var reader = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+reader.on("line", (line) => {
+    lines.push(line);
+});
+reader.on("close", () => {
+    let S = lines[0].split("");
+    let arr = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ];
+    for (let i = 0; i < arr.length; i++) {
+        if (S.includes(arr[i])) {
+        } else {
+            console.log(arr[i]);
+            break;
+        }
+    }
+});
